4035d8be76744b464d1626a6a76fada5e4bbd073
fadlinur <130953101+fadlinur@users.noreply.github.com>
2026-08-17 10:23:41 +0700

    Change table creation to use new dataset location

--- queries/master_analysis.sql
+++ queries/master_analysis.sql
@@ -1,43 +1,20 @@
-CREATE OR REPLACE TABLE `kimia_farma.analisa_tabel` AS
-
-WITH base_data AS (
-
-    SELECT 
-
-        t.transaction_id,
-
-        t.date,
-
-        t.branch_id,
-
-        c.branch_name,
-
-        c.kota,
-
-        c.provinsi,
-
-        c.rating AS rating_cabang,
-
-        t.customer_name,
-
-        t.product_id,
-
-        p.product_name,
-
-        t.price AS actual_price,
-
-        t.discount_percentage,
-
-        t.rating AS rating_transaksi
-
-    FROM `kimia_farma.kf_final_transaction` AS t
-
-    LEFT JOIN `kimia_farma.kf_kantor_cabang` AS c 
-
-        ON t.branch_id = c.branch_id
-
-    LEFT JOIN `kimia_farma.kf_product` AS p 
-
-        ON t.product_id = p.product_id
-
-),
+CREATE OR REPLACE TABLE `rakamin-kf-analytics-505311.kimia_farma.base_data` AS
+SELECT 
+    t.transaction_id,
+    t.date,
+    t.branch_id,
+    c.branch_name,
+    c.kota,
+    c.provinsi,
+    c.rating AS rating_cabang,
+    t.customer_name,
+    t.product_id,
+    p.product_name,
+    t.price AS actual_price,
+    t.discount_percentage,
+    t.rating AS rating_transaksi
+FROM `rakamin-kf-analytics-505311.kimia_farma.kf_final_transaction` AS t
+LEFT JOIN `rakamin-kf-analytics-505311.kimia_farma.kf_kantor_cabang` AS c 
+    ON t.branch_id = c.branch_id
+LEFT JOIN `rakamin-kf-analytics-505311.kimia_farma.kf_product` AS p 
+    ON t.product_id = p.product_id;
